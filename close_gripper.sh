dc780834a37a005c860d0cc1c64c8e5ed045be09
shivangg <xshivangx@gmail.com>
2018-05-30 19:09:49 +0530

    Add script for opening and closing gripper

---
+++ close_gripper.sh
@@ -0,0 +1,11 @@
+rostopic pub /arm_1/gripper_controller/position_command brics_actuator/JointPositions "poisonStamp:
+  originator: ''
+  description: ''
+  qos: 0.0
+positions:
+- timeStamp:
+    secs: 0
+    nsecs: 0
+  joint_uri: 'gripper_finger_joint_r'
+  unit: 'm'
+  value: 0.001"
---
+++ open_gripper.sh
@@ -0,0 +1,11 @@
+rostopic pub /arm_1/gripper_controller/position_command brics_actuator/JointPositions "poisonStamp:
+  originator: ''
+  description: ''
+  qos: 0.0
+positions:
+- timeStamp:
+    secs: 4564532
+    nsecs: 54645623
+  joint_uri: 'gripper_finger_joint_r'
+  unit: 'm'
+  value: 0.011"
